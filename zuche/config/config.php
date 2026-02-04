ea116d45f751df964618b345002697ca465f1b18
HLGoogle <hl273573221@gmail.com>
2026-02-04 14:58:31 +0800

    Delete zuche directory

--- zuche/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class config 
-{
-    public $hostname = "localhost";
-    public $username = "root";
-    public $password = "root";
-    public $database = "acore_auth";
-}
--- zuche/lib/mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class mysql extends PDO 
-{
-    public function __construct(config $config)
-    {
-        parent::__construct("mysql:host=$config->hostname;dbname=$config->database", $config->username, $config->password);
-    }
-}
